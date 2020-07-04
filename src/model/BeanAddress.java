97fe9c112c33d3a34257e13b242c6fac0309326c
Hsaeno <47032254+Hsaeno@users.noreply.github.com>
2020-07-04 08:06:50 +0800

    test

---
+++ src/model/BeanAddress.java
@@ -0,0 +1,77 @@
+package model;
+
+public class BeanAddress {
+    private int address_id;
+    private int order_id;
+    private int user_id;
+    private String province;
+    private String city;
+    private String region;
+    private String contact_person;
+    private String  contact_phoneNumber;
+
+    public int getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(int address_id) {
+        this.address_id = address_id;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getContact_person() {
+        return contact_person;
+    }
+
+    public void setContact_person(String contact_person) {
+        this.contact_person = contact_person;
+    }
+
+    public String getContact_phoneNumber() {
+        return contact_phoneNumber;
+    }
+
+    public void setContact_phoneNumber(String contact_phoneNumber) {
+        this.contact_phoneNumber = contact_phoneNumber;
+    }
+}
+
